340ddab231720219679c2567553e2b9fb9f99ce0
Nali1919 <my@email.com>
2022-10-24 17:36:35 +0200

    Concludo esercizio

---
+++ Interrogazione.php
@@ -0,0 +1,31 @@
+
+<?php
+
+define("DB_SEVERNAME", "localhost");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "root");
+define("DB_NAME", "db_university");
+define("DB_PORT", 3306);
+
+
+$connessione = new mysqli(DB_SEVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
+
+if( $connessione && $connessione->connect_error) {
+    echo "Error" . $connessione->connect_error;
+    die();
+}
+
+$sql = "SELECT * FROM `degrees`";
+
+if($result = $connessione->query($sql)){
+    if($result->num_rows > 0){
+        
+
+    while($degrees = $result->fetch_assoc()){
+        echo "<div>" . $degrees['name'] . " " . $degrees['level'] . " " . $degrees['address']. "</div>"; 
+        
+    }
+    }
+}
+
+?>
